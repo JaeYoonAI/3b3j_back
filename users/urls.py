42fb773c8e003aa5b7a4317169a128860abf2e4b
JaeYoonAI <minamljy@gmail.com>
2023-10-06 19:56:42 +0900

    users based on lecture

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from users import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path("signup/", views.UserView.as_view(), name="user_view"),
+    path("mock/", views.mockView.as_view(), name="mock_view"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from users.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
 # Create your views here.
+
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "가입완료!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(
+                {"message": f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class mockView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        return Response("get요청")
