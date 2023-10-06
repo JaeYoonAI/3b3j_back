6903e91ea10170ccd811cec3062d6ced90da913f
JaeYoonAI <minamljy@gmail.com>
2023-10-06 15:49:10 +0900

    add users app

--- alpha_critic/settings.py
+++ alpha_critic/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "users",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
