3d6851f4e4aaf061245639d4ba2b5855a4cdab56
개발왕김개발 <141221115+Team-Kawa@users.noreply.github.com>
2023-10-10 12:50:40 +0900

    Set STATICFILES_DIRS for AWS

--- alpha_critic/settings.py
+++ alpha_critic/settings.py
@@ -155,7 +155,10 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-
+# Set Statics for AWS
+STATICFILES_DIRS = {
+     os.path.join(BASE_DIR, 'static'),
+}
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
 
