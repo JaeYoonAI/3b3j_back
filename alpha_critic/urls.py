9316d552bd3226cf7b4c4782e64be1e2b48a5f17
개발왕김개발 <141221115+Team-Kawa@users.noreply.github.com>
2023-10-11 23:08:59 +0900

    add indexpath

--- alpha_critic/urls.py
+++ alpha_critic/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from alpha_critic.views import index
 
 urlpatterns = [
+    path('',index),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("contents/", include("contents.urls")),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
