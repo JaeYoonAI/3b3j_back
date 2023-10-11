7516964cb0d427286540ff676761d4c33df85319
JaeYoonAI <minamljy@gmail.com>
2023-10-12 01:31:27 +0900

    comments CRUD

--- contents/views.py
+++ contents/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
+from django.shortcuts import get_object_or_404
 from contents.models import Content, MovieContent, MusicContent, GameContent, Comment
 from contents.serializers import (
     ContentSerializer,
@@ -69,11 +70,23 @@ class CommentView(APIView):
 
 
 class CommentDetailView(APIView):
-    def put(self, request):
-        pass
+    def put(self, request, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            serializer = CommentCreateSerializer(comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
 
-    def delete(self, request):
-        pass
+    def delete(self, request, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
 
 
 class LikeView(APIView):
