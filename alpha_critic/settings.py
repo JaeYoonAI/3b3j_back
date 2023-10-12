33d26a99ddd50e75c5f350b90acedc5036a48704
JaeYoonAI <minamljy@gmail.com>
2023-10-12 21:05:30 +0900

    adding fetch

--- alpha_critic/settings.py
+++ alpha_critic/settings.py
@@ -46,6 +46,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -165,3 +166,5 @@ STATIC_URL = "/static/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDOA_URL = "/media/"
+
+CORS_ALLOW_ALL_ORIGINS = True
--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    # path("signup/", views.UserView.as_view(), name="user_view"),
+    path("signup_api/", views.UserView.as_view(), name="user_view"),
     path("mock/", views.mockView.as_view(), name="mock_view"),
     path(
         "api/token/",
