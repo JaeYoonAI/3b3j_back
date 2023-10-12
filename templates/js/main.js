016fee7df3198b8c55abc0afce469670f94fd709
개발왕김개발 <141221115+Team-Kawa@users.noreply.github.com>
2023-10-12 09:09:12 +0900

    Reviews JS

--- templates/js/main.js
+++ templates/js/main.js
@@ -24,3 +24,42 @@ let swiperCards = new Swiper(".card__content", {
     },
   },
 });
+/*===============Reviews JS===============*/
+let reviews = [];
+
+function displayReviews() {
+    const reviewsContainer = document.getElementById('reviews');
+    reviewsContainer.innerHTML = '';
+
+    reviews.forEach(review => {
+        const reviewDiv = document.createElement('div');
+        reviewDiv.className = 'review';
+        reviewDiv.innerHTML = `
+            <h3>${review.title}</h3>
+            <p><strong>Rating:</strong> ${review.rating}</p>
+            <p>${review.comment}</p>
+            <hr>
+        `;
+        reviewsContainer.appendChild(reviewDiv);
+    });
+}
+
+function addReview() {
+    const title = prompt('Enter review title:');
+    const rating = parseFloat(prompt('Enter rating (1-5):'));
+    const comment = prompt('Enter your review:');
+
+    if (title && !isNaN(rating) && rating >= 1 && rating <= 5 && comment) {
+        const review = { title, rating, comment };
+        reviews.push(review);
+        displayReviews();
+    } else {
+        alert('Invalid input. Please try again.');
+    }
+}
+
+// Initial reviews for demonstration
+reviews.push({ title: 'Great Product', rating: 5, comment: 'I love this product!' });
+reviews.push({ title: 'Could be Better', rating: 3, comment: 'Not bad, but could use improvements.' });
+
+displayReviews();
